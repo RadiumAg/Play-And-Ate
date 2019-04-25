9e0665cc654ad2cbbffcedd8984dc722f87fa080
ym <2774599064@qq.com>
2019-04-25 19:24:12 +0800

    Login Ang Register

---
+++ Play And Ate/FTZ.PlayAndAte.Models/LoginAndRegister/Role_UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTZ.PlayAndAte.Models.LoginAndRegister
+{
+    class Role_UserInfo
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ Play And Ate/FTZ.PlayAndAte.Models/LoginAndRegister/UserInfo_Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTZ.PlayAndAte.Models
+{
+    class UserInfo_Role
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string QQ { get; set; }
+        public string Phone { get; set; }
+        public int RoleId { get; set; }
+        public string Pwd { get; set; }
+    }
+}
